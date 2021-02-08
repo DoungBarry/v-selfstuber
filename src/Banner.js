68c4fe6c44137dc8171850dc012384efbd3d04cc
Barry Yu <doung1118@gmail.com>
2021-02-09 00:15:11 +0800

    updata : 文字排版rem css

--- src/Banner.js
+++ src/Banner.js
@@ -10,8 +10,9 @@ function Banner() {
   return (
    <header className="banner" style={{
     blackgrundSize: "cover", 
-    backgroundImage: `url("https://lh3.google.com/u/0/d/1cxDRBOTttqAt86kvQ8Sb7vaQEWr_v1wh=w2880-h1304-iv1")`,
+    backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
       blackgroundPosition: "center center",
+        // backgroundImage: `url("https://lh3.google.com/u/0/d/1cxDRBOTttqAt86kvQ8Sb7vaQEWr_v1wh=w2880-h1304-iv1")`,
     
   }}>
 
@@ -24,10 +25,20 @@ function Banner() {
         <button className="banner-button">List</button>
         </div>
         
-        <h1 className="banner-description">
-          This is test description
+        <h1 className="banner-descript">
+初學開發者碰到文字排版總是非常頭疼 , 
+這關係到RWD , 看起來是不是乾淨 .簡潔 , 舒服 , 大小適當
+通常都習慣用 px 作為尺寸設定 . 可是當網頁面及文字非常多的時候, 需要微調會非常麻煩. 尺寸比例不是改一個數值就好. 要整體都看起來有平衡感. 
+所以我會推薦使用 rem . 
+因為rem 和 ％ 很像都是相對參照Root 去改變大小 . 當文字微調時只要改變Root 就可以了 . 
+名稱為 ： Root + em =1 em 當父（上一層）沒有設定大小的預設是16px
+文字大小通常建議設定在0.8 rem .
+行距間隔 建議設定在 1 rem
 </h1>
-<div className="banner--fadebutton"></div>
+        <div className="banner--fadebutton"></div>
+        
+        {/* 漸層區域 ==> hover  */}
+
     </div>
   </header>
   )
